d3de62ee605c7e6862af24a56071c9b1f33fcfaf
vishalsharma1996 <vishalg148256@gmail.com>
2026-03-14 10:59:01 +0000

    Added chunk registry for citation grounding

--- src/loaders.py
+++ src/loaders.py
@@ -6,29 +6,34 @@ import os
 def load_paper_chunks(paper_path):
 
     section_path = paper_path / "chunk.parquet"
-    table_path = paper_path / "table_chunk.parquet"
+    table_path = paper_path / "table.parquet"
 
     chunks = []
+    chunks_registry = []
 
     if section_path.exists():
         df = pd.read_parquet(section_path)
         chunks.extend(df["text"].tolist())
+        chunks_registry.append({row['text']:[row['chunk_id'],row['metadata']] for _,row in df.iterrows()})
 
     if table_path.exists():
         df = pd.read_parquet(table_path)
         chunks.extend(df["text"].tolist())
+        chunks_registry.append({row['text']:[row['chunk_id'],row['metadata']] for _,row in df.iterrows()})
 
-    return chunks
+    return chunks,chunks_registry
 
 def load_dataset_chunks(path = 'parsed_docs'):
   topics = os.listdir(path)
   all_chunks = []
+  all_chunks_registry = []
   for topic in topics:
     topic_path = os.path.join(path,topic,'chunks_tables')
     root = Path(topic_path)
     for paper_dir in root.iterdir():
       if paper_dir.is_dir():
-        chunks = load_paper_chunks(paper_dir)
+        chunks,chunks_registry = load_paper_chunks(paper_dir)
         if chunks:
           all_chunks.extend(chunks)
-  return all_chunks
+          all_chunks_registry.extend(chunks_registry)
+  return all_chunks,all_chunks_registry
